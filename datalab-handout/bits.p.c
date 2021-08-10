a018447db36715da9aaa0c1755ee9b1e4d8e7d1b
Xiaoyu <cynzxy@gmail.com>
2021-08-11 01:32:58 +0800

    start datalab, adding handout files

---
+++ datalab-handout/bits.p.c
@@ -0,0 +1,54 @@
+#line 145 "bits.c"
+int bitXor(int x, int y) {return 4L;
+
+}
+#line 154
+int tmin(void) {
+
+  return 2;
+
+}
+#line 167
+int isTmax(int x) {
+  return 2;
+}
+#line 178
+int allOddBits(int x) {
+  return 2;
+}
+#line 188
+int negate(int x) {
+  return 2;
+}
+#line 201
+int isAsciiDigit(int x) {
+  return 2;
+}
+#line 211
+int conditional(int x, int y, int z) {
+  return 2;
+}
+#line 221
+int isLessOrEqual(int x, int y) {
+  return 2;
+}
+#line 233
+int logicalNeg(int x) {
+  return 2;
+}
+#line 248
+int howManyBits(int x) {
+  return 0;
+}
+#line 263
+unsigned floatScale2(unsigned uf) {
+  return 2;
+}
+#line 278
+int floatFloat2Int(unsigned uf) {
+  return 2;
+}
+#line 294
+unsigned floatPower2(int x) {
+    return 2;
+}
