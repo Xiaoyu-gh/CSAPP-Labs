7daa5cc87864de78cf0e5cfded7c16c456f3ee72
Xiaoyu <cynzxy@gmail.com>
2021-08-24 10:34:11 +0800

    implement parseArguments function in part A

--- cache-lab/csim.c
+++ cache-lab/csim.c
@@ -1,7 +1,73 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <math.h>
 
-int main()
-{
-    printSummary(0, 0, 0);
-    return 0;
+// global variable
+static int s, E, b;
+FILE *pfile; // pointer to FILE struct, used to read tracefile
+
+int main(int argc, char **argv) {
+    parseArguments(argc, argv);
+
+
+    struct cacheLine {
+        // since it's only for simulation
+        // and count hit and miss times
+        // "block" section is unneeded
+        bool valid;
+        unsigned tag;
+        int useCounter;
+    };
+    
+    // dynamically allocate memory for cache
+    // which is a 2D array of cacheline
+    struct cacheLine **cache = malloc(sizeof(cacheLine));
+
 }
+
+// Phase1: parse command line options and arguments
+// initialize cache size: s, E and b
+void parseArugument(int argc, char **argv) {
+    int opt;
+    // looping over each argument
+    while ((opt = getopt(argc, argv, "s:E:b:t:")) != -1) {
+        // determine which argument it's processing
+        // optarg is a char*, whitch stored option arguments
+        switch(opt) {
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 't':
+                // open tracefile,return pfile point to it
+                pfile = fopen(optarg, 'r');
+                break;
+            default:
+                printf("Wrong argument\n");
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+printSummary(0, 0, 0);
+    return 0;
+
