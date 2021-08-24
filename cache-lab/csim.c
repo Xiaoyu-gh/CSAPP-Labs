bd00167dbb9b9b53743a6a60c64ec5f74c58a296
Xiaoyu <cynzxy@gmail.com>
2021-08-24 15:21:02 +0800

    implement cacheInitialize function and error checking

--- cache-lab/csim.c
+++ cache-lab/csim.c
@@ -1,36 +1,44 @@
-#include "cachelab.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <math.h>
+// Cache Simulator:
+// use trace file as input
+// print out counts of hits, misses and evictions
+
+#include "cachelab.h" // required
+#include <stdio.h>  // IO
+#include <stdlib.h>  // library functions
+#include <unistd.h>  // dependency of getopt.h
+#include <getopt.h>  // use getopt function
+#include <math.h>  // use calculation like pow()
+
+
+// define data structures(begin with capital letter)
+struct CacheLine {
+    // since it's only for simulation
+    // and count hit and miss times
+    // "block" section is unneeded
+    bool valid;
+    uint64_t tag; // unsigned 64-bit int
+    int useCounter;
+};
+typedef CacheLine* CacheSet;
+typedef CachSet* Cache; // so Cach is Cacheline**
 
 // global variable
 static int s, E, b;
 FILE *pfile; // pointer to FILE struct, used to read tracefile
+Cache cache;
 
 int main(int argc, char **argv) {
     parseArguments(argc, argv);
-
-
-    struct cacheLine {
-        // since it's only for simulation
-        // and count hit and miss times
-        // "block" section is unneeded
-        bool valid;
-        unsigned tag;
-        int useCounter;
-    };
+    cacheInitialize();
     
-    // dynamically allocate memory for cache
-    // which is a 2D array of cacheline
-    struct cacheLine **cache = malloc(sizeof(cacheLine));
 
+    printSummary();
+    return 0;
 }
 
 // Phase1: parse command line options and arguments
-// initialize cache size: s, E and b
-void parseArugument(int argc, char **argv) {
+// initialize cache size: s, E and b(global variable)
+void parseAruguments(int argc, char **argv) {
     int opt;
     // looping over each argument
     while ((opt = getopt(argc, argv, "s:E:b:t:")) != -1) {
@@ -49,6 +57,10 @@ void parseArugument(int argc, char **argv) {
             case 't':
                 // open tracefile,return pfile point to it
                 pfile = fopen(optarg, 'r');
+                if (pfile == NULL) {
+                    printf("Open wrong trace file.\n");
+                    exit(1);
+                }
                 break;
             default:
                 printf("Wrong argument\n");
@@ -57,17 +69,44 @@ void parseArugument(int argc, char **argv) {
     }
 }
 
+void simulate() {
+    cacheInitialze();
 
+    char flag;
+    uint64_t address;
+    int size; // just for formatting, no actual use
 
+    // loop through all file
+    // each iteration process one line
+    // Note: fscanf will ignore space while reads stream
+    while (fscanf(pfile, "%c %x,%d", &flag, &address, &size) == 3) {
+        if (flag == 'I') continue; // skip instruction
+        switch (flag) {
+            case 'L':
+        }
 
+    }
+}
 
+// allocate space for cache and initialize it with 0
+void cacheInitialize() {
+    int S = pow(2, s); // number of sets
 
+    // allocate memory for cache
+    // CacheSet is pointer, whose size is 8
+    cache = (cache)malloc(sizeof(CacheSet) * S);
+    if (cache == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
 
-
-
-
-
-
-printSummary(0, 0, 0);
-    return 0;
-
+    // allocate memory for each CacheSet in cache
+    for (int i = 0; i < S; i++) {
+        // must use calloc but not malloc, to initialize with 0
+        cache[i] = (CacheSet)calloc(E, sizeof(CacheLine));
+        if (cache[i] == NULL) {
+            printf("Memory allocation failed.\n");
+            exit(1);
+        }
+    }
+}
